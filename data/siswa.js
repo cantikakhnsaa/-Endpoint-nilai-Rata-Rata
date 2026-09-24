4cec4b5bbb870fb6a117ec32ef1462b4bccce4a1
cantikakhnsaa <cantikakhrnns@gmail.com>
2026-09-24 14:26:32 +0700

    feat: add endpoint nilai rata-rata

---
+++ data/siswa.js
@@ -0,0 +1,24 @@
+const siswa = [
+    {
+        nama: "Cantika",
+        kelas: "12 PPLG 2",
+        nilai: [90, 78, 82]
+    },
+    {
+        nama: "Kala",
+        kelas: "12 PPLG 2",
+        nilai: [78, 85, 88]
+    },
+    {
+        nama: "Bima",
+        kelas: "12 PPLG 2",
+        nilai: [82, 87, 85]
+    },
+    {
+        nama: "Rea",
+        kelas: "12 PPLG 2",
+        nilai: [92, 89, 88]
+    },
+];
+
+export default siswa;
---
+++ server.js
@@ -0,0 +1,65 @@
+import express from "express";
+import siswa from "./data/siswa.js";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Backend API Nilai Rata-rata berjalan"
+  });
+});
+
+app.get("/nilai/rata-rata/:kelas", (req, res) => {
+  const kelas = req.params.kelas;
+
+  const orderBy = req.query.order_by
+    ? req.query.order_by.toUpperCase()
+    : "DESC";
+
+  if (orderBy !== "ASC" && orderBy !== "DESC") {
+    return res.status(400).json({
+      message: "order_by harus ASC atau DESC"
+    });
+  }
+
+  const siswaKelas = siswa.filter(
+    (data) => data.kelas.toLowerCase() === kelas.toLowerCase()
+  );
+
+  if (siswaKelas.length === 0) {
+    return res.status(404).json({
+      message: "Kelas tidak ditemukan"
+    });
+  }
+
+  const hasil = siswaKelas.map((data) => {
+    const total = data.nilai.reduce(
+      (jumlah, nilai) => jumlah + nilai,
+      0
+    );
+
+    const average = Number(
+      (total / data.nilai.length).toFixed(2)
+    );
+
+    return {
+      name: data.nama,
+      average: average
+    };
+  });
+
+  if (orderBy === "ASC") {
+    hasil.sort((a, b) => a.average - b.average);
+  }
+
+  if (orderBy === "DESC") {
+    hasil.sort((a, b) => b.average - a.average);
+  }
+
+  res.json(hasil);
+});
+
+app.listen(port, () => {
+  console.log(`Server Berjalan di http://localhost:${port}`);
+});
