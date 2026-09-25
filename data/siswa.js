6874692d3f3a51ae7c8f486f47d73953d7585f42
cantikakhnsaa <cantikakhrnns@gmail.com>
2026-09-25 10:45:46 +0700

    perubahan data siswa

--- data/siswa.js
+++ data/siswa.js
@@ -1,17 +1,22 @@
 const siswa = [
     {
         nama: "Cantika",
-        kelas: "12 PPLG 2",
+        kelas: "12 PPLG 4",
         nilai: [90, 88, 82]
     },
     {
+        nama: "Tanjiro",
+        kelas: "12 PPLG 4",
+        nilai: [90, 88, 92]
+    },
+    {
         nama: "Kala",
-        kelas: "12 PPLG 2",
+        kelas: "12 PPLG 4",
         nilai: [78, 85, 88]
     },
     {
         nama: "Bima",
-        kelas: "12 PPLG 2",
+        kelas: "12 PPLG 3",
         nilai: [82, 87, 85]
     },
     {
@@ -19,6 +24,11 @@ const siswa = [
         kelas: "12 PPLG 2",
         nilai: [92, 89, 88]
     },
+    {
+        nama: "Zenitsu",
+        kelas: "12 PPLG 2",
+        nilai: [85, 90, 87]
+    }
 ];
 
 export default siswa;
